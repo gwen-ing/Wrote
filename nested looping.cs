77733935c493579e25d89f08d1c9e6e291707836
Gweb <jehuthedelusional@gmail.com>
2025-06-02 19:50:24 +0800

    Create nested looping.cs

---
+++ nested looping.cs
@@ -0,0 +1,9 @@
+//nested looping
+
+for (int seats = 0; seats < 11; seats++)
+{
+    for (char word = 'a'; word < 'k'; word++)
+    {
+        Console.WriteLine($"The loops are ({seats}, {word})");    
+    }
+}
