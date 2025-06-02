76c86aca64821692bb5efc516aade5ae48d94820
Gweb <jehuthedelusional@gmail.com>
2025-06-02 19:42:41 +0800

    Create simple looping with branching.cs

---
+++ simple looping with branching.cs
@@ -0,0 +1,17 @@
+//looping 2 with branching
+
+int p = 5;
+int i = 0;
+
+if (p > 4)
+{
+    while (i < 21)
+    {
+        Console.WriteLine($"Counts to {i}");
+        i++;
+    }
+}
+else
+{
+    Console.WriteLine($"Can't count probably an error");
+}
