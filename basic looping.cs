7d737a73e80b857b555a83423720aa9d014c0c68
Gweb <jehuthedelusional@gmail.com>
2025-06-02 19:36:20 +0800

    Create basic looping.cs

---
+++ basic looping.cs
@@ -0,0 +1,19 @@
+//loops
+/*int counter = 0;
+while (counter < 11)
+{
+    Console.WriteLine($"\ncounting to {counter}");
+    counter++;
+}*/
+
+/*int counter2 = 0;
+do
+{
+    Console.WriteLine($"\ncounting to {counter2}");
+    counter2++;
+} while (counter2 < 11); */
+
+for (int counter3 = 0; counter3 < 10; counter3++)
+{
+    Console.WriteLine($"\ncounting to {counter3}");
+}
