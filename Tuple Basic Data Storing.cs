427d9355ab43a1ba2679bbac30f639dcf3b9aaa2
Gweb <jehuthedelusional@gmail.com>
2025-06-02 17:53:34 +0800

    Update Tuple Basic Data Storing.cs

--- Tuple Basic Data Storing.cs
+++ Tuple Basic Data Storing.cs
@@ -1,14 +1,14 @@
 //tuples
 var pt = (x: 2, y: 4);
-var slope = (double)pt.y / (double)pt.x;
-
 pt.y = pt.y + 2;
+
+var slope = (double)pt.y / (double)pt.x;
 
 Console.WriteLine($"A line from origin of the point {pt} has a slope of {slope}\n");
 
 //tuple with brackets changing int number to = N/A
 var pt2 = pt with { y = 10 };
-var slope2 = (double)pt.y / (double)pt.x;
+var slope2 = (double)pt2.y / (double)pt2.x;
 
 Console.WriteLine($"The point pt2 is at {pt2} and slope is {slope2}\n");
 
