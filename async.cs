9561c18ea347f3628c8e6301668cc33037218447
Gweb <jehuthedelusional@gmail.com>
2025-06-03 18:57:30 +0800

    Update async.cs

--- async.cs
+++ async.cs
@@ -10,3 +10,20 @@ class Program
         return 0;
     }
 }   
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        if (args.Length == 0)
+        {
+            Console.WriteLine("Argument is passing");
+        }
+
+        long num = Int64.Parse(args[0]);
+        long num2 = long.Parse(args[1]);
+        long num3 = Convert.ToInt64(num);
+
+
+    }
+}   
