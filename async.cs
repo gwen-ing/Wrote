7336045d1c01925916a40065536d0a8d9fd1fa2c
Gweb <jehuthedelusional@gmail.com>
2025-06-03 18:44:22 +0800

    Create async.cs

---
+++ async.cs
@@ -0,0 +1,12 @@
+class Program
+{
+    static async Task<int> Main(string[] args)
+    {
+        return await AsyncConsoleWork();
+    }
+
+    public static async Task<int> AsyncConsoleWork()
+    {
+        return 0;
+    }
+}   
