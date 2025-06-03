8dfa177b84f48b66b5234ef36b5631e983f80c87
Gweb <jehuthedelusional@gmail.com>
2025-06-03 17:47:47 +0800

    Create overview learning.cs

---
+++ overview learning.cs
@@ -0,0 +1,45 @@
+using System;
+
+
+
+namespace Cool
+{
+    class Program
+    {
+
+    }
+
+    struct Container
+    {
+
+    }
+
+    interface Faces
+    {
+
+    }
+
+    delegate int Elegant();
+
+    enum Enumeration
+    {
+
+    }
+
+    namespace nested
+    {
+        struct hi
+        {
+
+        }
+
+        class program
+        {
+            static void Main(string[] hi)
+            {
+                Console.WriteLine("Hello World");
+            }
+        }
+    }
+
+}
